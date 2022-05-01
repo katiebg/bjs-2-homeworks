2fec5e661cfb1c6565385e5a31b2cbabe504f76e
katiebg <97286492+katiebg@users.noreply.github.com>
2022-05-02 00:48:21 +0300

    Add files via upload

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  return ((arr1.length === arr2.length) && (arr1.every((item, idx) => item === arr2[idx])));
 
-  return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
-
-  return resultArr; // array
+  return ((arr.filter((item) => (item > 0 && item % 3 === 0))).map((item) => item * 10));
 }
