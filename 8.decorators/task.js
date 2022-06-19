d1ff0072255203c42533136d3e2e2b43595555a6
katiebg <97286492+katiebg@users.noreply.github.com>
2022-06-19 21:32:56 +0300

    Add files via upload

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,42 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
-}
+
+  function cachingDecoratorNew(func) {
+    let cache = [];
+    
+    function wrapper(...args) {
+        const hash = args.join(","); // получаем правильный хэш
+        let idx = cache.findIndex((item)=> item = hash ); // ищем элемент, хэш которого равен нашему хэшу
+        if (idx !== -1 ) { // если элемент не найден
+            console.log("Из кэша: " + cache[idx]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + cache[idx];
+        }
+    
+        let result = func(...args); // в кэше результата нет - придётся считать
+        cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
+    }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(f, ms) {
+  let timeout;    
+  let flag = true;
+  return function (...args) 
+  {        
+    clearTimeout(timeout);        
+    timeout = setTimeout(() => {   
+      if (!flag) {
+        f.apply(this, args); 
+        flag = true;
+      }         
+    }, ms);
+  };
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  
 }
