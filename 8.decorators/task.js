7d929562a47516b1e3f60b85bfa9b6e7e2a27498
katiebg <97286492+katiebg@users.noreply.github.com>
2022-07-07 16:50:40 +0300

    Add files via upload

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,14 +4,14 @@
     
     function wrapper(...args) {
         const hash = args.join(","); // получаем правильный хэш
-        let idx = cache.findIndex((item)=> item = hash ); // ищем элемент, хэш которого равен нашему хэшу
-        if (idx !== -1 ) { // если элемент не найден
-            console.log("Из кэша: " + cache[idx]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-            return "Из кэша: " + cache[idx];
+        let idx = cache.findIndex((item, idx)=> item[idx].hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
+        if (idx !== -1 ) { // если элемент  найден
+            console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + cache[idx].result;
         }
     
         let result = func(...args); // в кэше результата нет - придётся считать
-        cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+        cache.push({hash: this.hash, result: this.result}) ; // добавляем элемент с правильной структурой
         if (cache.length > 5) { 
           cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
         }
@@ -22,11 +22,23 @@
     }
 
 
+    function debounceDecoratorNew(f, ms) {
+      let isCooldown = false;
+      return function wrapper() {
+        if (isCooldown) return;
+        f.apply(this, null);
+        isCooldown = true;
+        setTimeout(() => isCooldown = false, ms);
+      };
+    }
+    
+
 function debounceDecoratorNew(f, ms) {
   let timeout;    
   let flag = true;
-  return function (...args) 
-  {        
+  return function (...args) {  
+    f.apply(this, args); 
+    flag = true;      
     clearTimeout(timeout);        
     timeout = setTimeout(() => {   
       if (!flag) {
@@ -37,6 +49,7 @@ function debounceDecoratorNew(f, ms) {
   };
 }
 
+
 function debounceDecorator2(func) {
-  
+
 }
