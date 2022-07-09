31e5b4203e719c5ce6f4afce8f52342c058bc9fa
katiebg <97286492+katiebg@users.noreply.github.com>
2022-07-09 16:53:56 +0300

    Add files via upload

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,55 +1,47 @@
 
-  function cachingDecoratorNew(func) {
-    let cache = [];
-    
-    function wrapper(...args) {
-        const hash = args.join(","); // получаем правильный хэш
-        let idx = cache.findIndex((item, idx)=> item[idx].hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
-        if (idx !== -1 ) { // если элемент  найден
-            console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-            return "Из кэша: " + cache[idx].result;
-        }
-    
-        let result = func(...args); // в кэше результата нет - придётся считать
-        cache.push({hash: this.hash, result: this.result}) ; // добавляем элемент с правильной структурой
-        if (cache.length > 5) { 
-          cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
-        }
-        console.log("Вычисляем: " + result);
-        return "Вычисляем: " + result;  
-    }
-    return wrapper;
+function cachingDecoratorNew(func) {
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(","); // получаем правильный хэш
+    let idx = cache.findIndex((item, idx) => item[idx].hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1) { // если элемент  найден
+      console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + cache[idx].result;
     }
 
-
-    function debounceDecoratorNew(f, ms) {
-      let isCooldown = false;
-      return function wrapper() {
-        if (isCooldown) return;
-        f.apply(this, null);
-        isCooldown = true;
-        setTimeout(() => isCooldown = false, ms);
-      };
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({ hash: hash, result: result }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
     }
-    
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
+}
 
 function debounceDecoratorNew(f, ms) {
-  let timeout;    
+  let timeout;
+  f(...args);
   let flag = true;
-  return function (...args) {  
-    f.apply(this, args); 
-    flag = true;      
-    clearTimeout(timeout);        
-    timeout = setTimeout(() => {   
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
       if (!flag) {
-        f.apply(this, args); 
+        f.apply(this, args);
         flag = true;
-      }         
+      }
     }, ms);
   };
 }
 
 
 function debounceDecorator2(func) {
-
+  let count = 0;
+  function wrapper(...args) {
+    count++;
+    debounceDecoratorNew.call(this, ...args);
+  }
+  return wrapper;
 }
